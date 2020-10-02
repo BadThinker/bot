847cf898146ba24fcf4de5674c3992735f787a14
BadThinker <artemcopywriting@gmail.com>
2020-10-02 20:07:20 +0300

    Добавлен модуль base.py в main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from vk_api.longpoll import VkLongPoll, VkEventType         #Для работы
 from src.user import User			#Модуль с описанием пользователя
 from src.api import vk				#Авторизованный объект 'vk' для работы с VK-API
 from src.analysis import Analysis	#Объект анализирующий сообщение пользователя
+from src.base import Base			#Объект с базой данных
 
 while(True):
 	longpoll = VkLongPoll(vk)		#Работа с сообщениями
@@ -12,3 +13,6 @@ while(True):
 				user = User(event.message, event.user_id)	#Создали пользователя
 				analysis = Analysis(event.message)			#Анализатор сообщения
 				analysis.analysed()							#Проанализировали текст
+				base = Base()								#Создали базу данных
+				print(base.all_institutes)		
+				print(base.all_groups)
