27c9b0458a497718aec86c4d52b27e9f337ac106
BadThinker <artemcopywriting@gmail.com>
2020-10-02 20:35:38 +0300

    Написан примерный костяк функции analysed

--- src/analysis.py
+++ src/analysis.py
@@ -1,8 +1,20 @@
 from src.user import User
+from src.base import Base
+from src.consts import ALL_COURSES, ALL_DAYS
+from src.text import isUniversity, isInstitute, isCourse, isGroup, isDay
 
 class Analysis:
 	def __init__(self, message):
 		self.text = message			#Сообщение отправленное пользователя
 	
+	#Анализируем сообщение пользователя и возвращаем сигнал:
 	def analysed(self):
-		print("Ага, я проанализировал этот текст: ", self.text)
+		base = Base()				#Создали базу данных
+
+		if(isUniversity(self.text, base.all_universitys)):	return 1
+		if(isInstitute(self.text, base.all_institutes)):	return 2
+		if(isCourse(self.text, ALL_COURSES)):				return 3
+		if(isGroup(self.text, base.all_groups)):			return 4
+		if(isDay(self.text, ALL_DAYS)):						return 5
+		#***FIX IT****(Спам обрабатывется пока что так)
+		else 												return 0					
