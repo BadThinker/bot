96749bdac4d1952a922aa7a49ce6d2e16d8b71bf
BadThinker <artemcopywriting@gmail.com>
2020-10-02 19:59:20 +0300

    Добавлены пути

--- src/consts.py
+++ src/consts.py
@@ -3,3 +3,5 @@ GROUP_ID = 191569268
 #API-ключ от группы с ботом(Тест)
 TOKEN = "dbe999cb110c88064cad70f08d679ff5d98c63a883b2087c85b638bea84bc935428ce05e28eafc9b32d57"
 
+#PATH_TO_UNIVERSITY = "../data/"	#Путь до университетов(Пока не надо, институт только один)
+PATH_TO_INSTITUTES = "/data"	 	#Путь до институтов
