5dfd1d1c2ba54d56d282fd735931c61430f212d6
BadThinker <artemcopywriting@gmail.com>
2020-10-02 19:15:57 +0300

    Созданы функции-пустышки в text.py

---
+++ src/text.py
@@ -0,0 +1,23 @@
+#Это университет?
+def isUniversity(text):
+	pass
+
+#Это институт?
+def isInstitute(text):
+	pass
+
+#Это курс?
+def isCourse(text):
+	pass
+
+#Это группа?
+def isGroup(text):
+	pass
+
+#Это день недели?
+def isDay(text):
+	pass
+
+#Это спам(т.е. сообщение, не поддающееся командам)
+def isSpam(text):
+	pass
