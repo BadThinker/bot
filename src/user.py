77f0a1238ca6e9cae32f5f84df95add5943b529b
BadThinker <artemcopywriting@gmail.com>
2020-10-02 23:05:44 +0300

    Добавлены новые поля у класса User

--- src/user.py
+++ src/user.py
@@ -1,12 +1,16 @@
 from src.api import vk
 from src.consts import GROUP_ID
 
-
 class User:
 	def __init__(self, message, id):
 		self.message = message				#Текущее сообщение
 		self.id = id						#ID-пользователя
 		self.inGroup = self.isMember()		#Состоит ли он в группе?
+		self.university = None				#Университет пользователя
+		self.institute = None				#Институт пользователя
+		self.course = None					#Курс пользователя
+		self.group = None					#Группа пользователя
+		self.day = None						#Выбранный пользователем день
 
 	#Проверяет является ли пользователь участником
 	def isMember(self):
