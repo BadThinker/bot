e6488fe94d9217573d3a1754013a59a2383b1724
BadThinker <artemcopywriting@gmail.com>
2020-10-02 22:37:52 +0300

    Описаны примитивные шаблоны по распознованию текста

--- src/text.py
+++ src/text.py
@@ -1,19 +1,40 @@
-#Это университет?
+import re							#Регулярные выражения
+from src.consts import ALL_DAYS
+
+#Это университет? (пока что не нужная функция, т.к. университет только один - ВлГУ)
 def isUniversity(text, all_universitys):
 	pass
 
 #Это институт?
 def isInstitute(text, all_institutes):
-	pass
+	text = text.upper()
+	for institute in all_institutes:
+		if institute == text:	return True
 
-#Это курс?
+	return False
+
+#Это курс какого то университета?
 def isCourse(text, all_courses):
-	pass
+	text = text.upper().split(" ")
+	if(len(text) == 2):
+		#******FIX IT*******(Сделать проверку на институт)
+		if text[0].find("КУРС") != -1 or text[1].find("КУРС") != -1:		return True
+	return False
 
 #Это группа?
-def isGroup(text, all_groups):
-	pass
+def isGroup(text):
+	result = re.search(r"\w{1,3}[- ]\d\d\d.{0,1}|\w{1,3}\d\d\d.{0,1}", text)
+	if result != None and result.group(0) == text: return True
+	return False
 
-#Это день недели?
-def isDay(text, all_days):
-	pass
+#Это расписание на какой то день?
+def isDay(text):
+	text = text.upper().split(" ")
+	if(len(text) > 1):
+		for word in text:
+			for day in ALL_DAYS:
+				if word == day:
+					for word2 in text:
+						if isGroup(word2):	return True 
+	
+	return False
